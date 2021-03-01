84e4b61bbf0bed67dcea3c89c1a668ce853ca2a6
kohei sakurai <ks19ks19ks19@gmail.com>
2021-03-01 16:55:45 +0900

    add button to delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:edit, :update]
+  before_action :set_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all.order(created_at: :desc)
@@ -27,6 +27,11 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: 'Deleted!'
   end
 
   private
