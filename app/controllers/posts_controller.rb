643a8a5be107aa841674514c413acac367a50d14
kohei sakurai <ks19ks19ks19@gmail.com>
2021-03-01 17:41:12 +0900

    add confirmation function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,10 +11,14 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    if @post.save
-      redirect_to posts_path, notice: 'Posted!'
-    else
+    if params[:back]
       render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: 'Posted!'
+      else
+        render :new
+      end
     end
   end
 
@@ -32,6 +36,11 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     redirect_to posts_path, notice: 'Deleted!'
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
   end
 
   private
